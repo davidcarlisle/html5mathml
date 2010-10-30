80f716bfb243a35ec6d50aeda01169fc0a9e382a
d.p.carlisle <d.p.carlisle@36164553-b982-289f-f2b4-e228a9b7a29f>
2010-10-30 17:31:49 +0000

    add

---
+++ operamathml.js
@@ -0,0 +1,12 @@
+// operamathml.js
+// d.p.carlisle@gmail.com
+// may be used under MIT license
+// http://www.opensource.org/licenses/mit-license.php
+
+
+
+
+if ( navigator.userAgent.match(/Presto\/2\.6/)) {
+  document.write("<link rel=\"stylesheet\" href=\"operamathml.css\" type=\"text/css\">");
+    }
+
