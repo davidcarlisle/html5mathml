6bd277220ba9c6f5757ffc11c432d0dd5ed04df1
d.p.carlisle <d.p.carlisle@36164553-b982-289f-f2b4-e228a9b7a29f>
2011-07-03 22:32:58 +0000

    ie 10

--- trunk/iemathml.js
+++ trunk/iemathml.js
@@ -27,10 +27,54 @@ function domath() {
 }
 
 
+function convertie(node) {
+  if (node.nodeType==1) {
+    var newnode = 
+      document.createElement("m:" + node.nodeName);
+    for(var i=0; i < node.attributes.length; i++)
+      newnode.setAttribute(node.attributes[i].nodeName,
+                           node.attributes[i].nodeValue);
+    var cnode = [];
+    for( var i = 0; i <node.childNodes.length; i++ ) {
+        cnode[cnode.length] = node.childNodes[i];
+    }
+    for (var i=0; i<cnode.length; i++) {
+        newnode.appendChild(convertie(cnode[i]));
+    }
+    return newnode;
+  }
+  else return node;
+}
+
+function domath10() {
+          var mp = document.createElement("object");
+          mp.id = "mathplayer";
+          mp.classid = "clsid:32F66A20-7614-11D4-BD11-00104BD3F987";
+          document.getElementsByTagName("head")[0].appendChild(mp);
+          document.namespaces.add("m","http://www.w3.org/1998/Math/MathML");
+          document.namespaces.m.doImport("#mathplayer");
+ var mmlnodea = document.getElementsByTagName("math");
+ var mmlnode = [];
+  for( var i = 0; i < mmlnodea.length; i++ ) {
+    mmlnode[mmlnode.length] = mmlnodea[i];
+  }
+  for (var i=0; i<mmlnode.length; i++) {
+   
+ mmlnode[i].parentNode.replaceChild(convertie(mmlnode[i]),mmlnode[i]);
+
+  }
+}
+
+
 if (navigator.userAgent.match(/MSIE [6-9]/)) {
 document.write("<object id=\"mmlFactory\" classid=\"clsid:32F66A20-7614-11D4-BD11-00104BD3F987\"></object><?import namespace=\"m\" implementation=\"#mmlFactory\"?>");
 //window.onload = domath;
 window.attachEvent("onload", domath);
     }
 
+
+
+if (navigator.userAgent.match(/MSIE 1[0-9]/)) {
+    window.onload=domath10;
+    }
 
